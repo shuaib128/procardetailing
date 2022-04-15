190d4ef68007d83a1fb819d0a9499e38455eb07e
shuaib128 <shuaibahamed77@gmail.com>
2022-04-15 11:08:06 -0700

    relize

--- procardetailing/settings.py
+++ procardetailing/settings.py
@@ -141,3 +141,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
