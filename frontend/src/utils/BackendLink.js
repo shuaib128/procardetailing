3d044ba156ca8034b510bd0790288ca0680ae792
shuaib128 <shuaibahamed77@gmail.com>
2022-04-16 23:44:39 -0700

    changed 2

--- frontend/src/utils/BackendLink.js
+++ frontend/src/utils/BackendLink.js
@@ -1,2 +1,2 @@
-export const BackendLink = "https://procarbackend.herokuapp.com"
+export const BackendLink = "https://procardetailing.herokuapp.com"
 // export const BackendLink = "http://localhost:8000"
